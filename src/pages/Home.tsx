b2b2db9f7ff0aa50201ab86e1087fbf0934cb911
sonjasviden <sonja.sviden@live.com>
2023-05-27 21:21:04 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 
 function Home() {
@@ -10,7 +11,9 @@ function Home() {
                     <h1>Hi, I'm Sonja Svidén</h1>
                     <h2>Frontend developer with knowledge in UX/UI design</h2>
                     <p>Working with my hands to make magic happen on the internet. View my
-                        <button><a href="/projects">Projects</a></button>,
+                        <button><a href="/projects">
+                            <Link to="/projects">Projects</Link>
+                        </a></button>,
                         <button><a href="/about">About</a></button>,
                         <button><a href="/resume">Resumé</a></button>, or send me an email at either
                         <button><a href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the
