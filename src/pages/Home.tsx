76743c5da6b1ed3a90ad8a1b494cb02d35d1d12f
sonjasviden <sonja.sviden@live.com>
2023-05-27 21:13:26 +0200

    changed path

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -9,12 +9,11 @@ function Home() {
                 <div>
                     <h1>Hi, I'm Sonja Svidén</h1>
                     <h2>Frontend developer with knowledge in UX/UI design</h2>
-                    <p>Working with my hands to make magic happen on the internet. View my <button><a
-                        href="/pages/projects.html">Projects</a></button>,
-                        <button><a href="/about">About</a></button>, <button><a
-                            href="/resume">Resumé</a></button>, or
-                        send me an email at either <button><a
-                            href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the
+                    <p>Working with my hands to make magic happen on the internet. View my
+                        <button><a href="/projects">Projects</a></button>,
+                        <button><a href="/about">About</a></button>,
+                        <button><a href="/resume">Resumé</a></button>, or send me an email at either
+                        <button><a href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the
                         <button><a href="/contact">contact form</a></button>.
                     </p>
                 </div>
