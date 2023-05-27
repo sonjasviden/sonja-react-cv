dc06454c6a33c9c7b6d6cfd94ab5a4ba0fa08232
sonjasviden <sonja.sviden@live.com>
2023-05-27 21:09:23 +0200

    changes links path in homepage

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -11,11 +11,11 @@ function Home() {
                     <h2>Frontend developer with knowledge in UX/UI design</h2>
                     <p>Working with my hands to make magic happen on the internet. View my <button><a
                         href="/pages/projects.html">Projects</a></button>,
-                        <button><a href="/pages/about.html">About</a></button>, <button><a
-                            href="/pages/resume.html">Resumé</a></button>, or
+                        <button><a href="/about">About</a></button>, <button><a
+                            href="/resume">Resumé</a></button>, or
                         send me an email at either <button><a
                             href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the
-                        <button><a href="/pages/contact.html">contact form</a></button>.
+                        <button><a href="/contact">contact form</a></button>.
                     </p>
                 </div>
                 <div>
