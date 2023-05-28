e60e5e5432c22627be3d496d4c110d270e373560
sonjasviden <sonja.sviden@live.com>
2023-05-28 02:25:56 +0200

    commit

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,20 +10,16 @@ function Home() {
                 <div>
                     <h1>Hi, I'm Sonja Svidén</h1>
                     <h2>Frontend developer with knowledge in UX/UI design</h2>
-                    <p>Working with my hands to make magic happen on the internet. View my
-                        <button><a href="/projects">
-                            <Link to="/projects">Projects</Link>
-                        </a></button>,
-                        <button><a href="/about">
-                            <Link to="/about">About</Link>
-                        </a></button>,
-                        <button><a href="/resume">
-                            <Link to="/resume">Resume</Link>
-                        </a></button>, or send me an email at either
-                        <button><a href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the
-                        <button><a href="/contact">
-                            <Link to="/contact">Contact</Link>
-                        </a></button>.
+                    <p>Working with my hands to make magic happen on the internet. View my <button><a href="/projects">
+                        <Link to="/projects">Projects</Link>
+                    </a></button>, <button><a href="/about">
+                        <Link to="/about">About</Link>
+                    </a></button>, <button><a href="/resume">
+                        <Link to="/resume">Resume</Link>
+                    </a></button>, or send me an email at either <button>
+                            <a href="mailto:sonja.sviden@live.com">sonja.sviden@live.com</a></button> or fill in the <button><a href="/contact">
+                                <Link to="/contact">contact form</Link>
+                            </a></button>.
                     </p>
                 </div>
                 <div>
