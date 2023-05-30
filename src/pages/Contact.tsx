ff6efb0f46ed8d8cd7abf4264b19e6c44c7054ca
sonjasviden <sonja.sviden@live.com>
2023-05-30 23:12:00 +0200

    responsivity

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,6 @@
 import { useForm } from '@formspree/react';
 import { motion } from 'framer-motion';
+import { Link } from 'react-router-dom';
 
 function Contact() {
     const [state, handleSubmit] = useForm("xlekaobw");
@@ -12,6 +13,16 @@ function Contact() {
                 <h2>Thank you!</h2>
                 <p>I'll get back to you shortly</p>
 
+                <div className="back-btn">
+                    <a href="/home">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor"
+                            className="bi bi-arrow-left" viewBox="0 0 16 16">
+                            <path fill-rule="evenodd"
+                                d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8z" />
+                        </svg>
+                        <Link to="/">Go back to home page</Link>
+                    </a>
+                </div>
             </div>
         )
     }
